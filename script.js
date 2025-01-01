ae552b472bcc5e3d36cc33a58a58753631ed9189
folushojimi <iamrblogger@gmail.com>
2025-01-01 13:04:33 +0100

    image responsiveness done

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const img = document.querySelector(".web3")
+// const img = document.querySelector(".web3")
  
 
-function updateImageSource(){
-   // If the viewport width is greater than 1024px (desktop)
-   if (window.innerWidth >= 1024) {
-    img.src = 'image-web-3-desktop.jpg';  // Change to desktop image
-} else {
-    img.src = 'image-web-3-mobile.jpg';   // Change to mobile image
-}
-}
+// function updateImageSource(){
+//    // If the viewport width is greater than 1024px (desktop)
+//    if (window.innerWidth >= 1024) {
+//     img.src = 'image-web-3-desktop.jpg';  // Change to desktop image
+// } else {
+//     img.src = 'image-web-3-mobile.jpg';   // Change to mobile image
+// }
+// }
   
-// Call the function initially
-updateImageSource();
+// // Call the function initially
+// updateImageSource();
 
-// Add an event listener to update image when resizing the window
-window.addEventListener('resize', updateImageSource);  
+// // Add an event listener to update image when resizing the window
+// window.addEventListener('resize', updateImageSource);  
